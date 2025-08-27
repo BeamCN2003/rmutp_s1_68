221559d1a8449034714d464fe8fa57ca15e41fa9
Pirapat Papat <pirapat-p@rmutp.ac.th>
2025-08-27 16:41:49 +0700

    update  output  sehema

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,23 @@
-import {Hono} from "hono";
+import { Hono } from "hono";
+import { PrismaClient } from "../generated/prisma/client";
+//import { PrismaClient }from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
 
+app.get("/", (c) => c.text("Hello World"));
 
-app.get("/", (c) => c.text("Hello World")); 
-app.get("/about",(c)=>{
-    return c.json({
-        message: " Pirapat Papat"
-    });
+app.get("/about", (c) => {
+  return c.json({
+    message: "Pirapat Papat",
+  });
 });
+
+app.get("/profile", async (c) => {
+    //login
+  const profiles = prisma.profile.findMany();
+  return c.json(profiles);
+});
+
 export default app;
