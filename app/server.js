3aff21abc571a40b6a769e3fec2d927cf7155324
พิรพัฒน์ ผาพัด <pirapat-p@rmutp.ac.th>
2025-08-27 15:01:33 +0700

    Delete app/server.js

--- app/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var node_server_1 = require("@hono/node-server");
-var index_js_1 = require("./index.js");
-(0, node_server_1.serve)(index_js_1.default, function (info) {
-    console.log("Sever is runnig on localhost:".concat(info.port));
-});
