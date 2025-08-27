dfb816b06aff34fcff63ad6578c046383cd871ca
Pirapat Papat <pirapat-p@rmutp.ac.th>
2025-08-27 15:00:58 +0700

    delete

--- app/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var node_server_1 = require("@hono/node-server");
-var index_1 = require("./index");
-(0, node_server_1.serve)(index_1.default, function (info) {
-    console.log("Sever is runnig on ".concat(info.port));
-});
