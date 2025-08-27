2eece3269738a7d3222b6ba960b795814f1439f6
Pirapat Papat <pirapat-p@rmutp.ac.th>
2025-08-27 15:16:04 +0700

    update server

--- app/server.ts
+++ app/server.ts
@@ -2,5 +2,5 @@ import { serve } from "@hono/node-server";
 import app from "./index";
 
 serve(app,(info)=>{
-    console.log(`Sever is runnig on ${info.port}`);
+    console.log(`Sever is runnig on http://localhost:${info.port}`);
 });
