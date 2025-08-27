bbc6ff5299ffe71dfdb1eeb62e32a4d909c55220
พิรพัฒน์ ผาพัด <pirapat-p@rmutp.ac.th>
2025-08-27 15:02:32 +0700

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-const app = new Hono();
-app.get("/", (c) => c.text("Hello World"));
-app.get("/about", (c) => {
-    return c.json({
-        message: " Pirapat Papat"
-    });
-});
-export default app;
--- dist/server.js
+++
@@ -1,5 +0,0 @@
-import { serve } from "@hono/node-server";
-import app from "./index.js";
-serve(app, (info) => {
-    console.log(`Sever is runnig on localhost:${info.port}`);
-});
