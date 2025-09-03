c28ea4d992d66e8b76e0e6a69c1506dfe60745f3
Pirapat Papat <pirapat-p@rmutp.ac.th>
2025-09-03 16:36:20 +0700

    password

--- app/index.ts
+++ app/index.ts
@@ -1,23 +1,35 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
-
-//import { PrismaClient } from "../generated/prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World"));
-
-app.get("/about", (c) => {
-  return c.json({
-    message: "Pirapat Papat",
-  });
-});
-
+app.get("/", (c) => c.text("Pirapat papat"));
 app.get("/profile", async (c) => {
-  const profiles = await prisma.profile.findMany();
-  return c.json(profiles);
+    //get data from db
+    const profiles = await prisma.profile.findMany();
+    //response
+    return c.json({
+        message: "get data completed",
+        data: profiles
+    }, 200);
+});
+app.post("/profile", async (c) => {
+    //logic to create a new profile
+    const body = await c.req.json();
+    console.log('input of profile ', body);
+    console.log('body.password(original) ', body.password);
+
+    //encode password
+    const passwordHash = await bcrypt.hash(body.password, 18);
+    console.log('hash.password(after))',passwordHash);
+    //save to db
+    //output response
+    return c.json({
+        message: "create profile completed"
+    });
 });
 
 export default app;
