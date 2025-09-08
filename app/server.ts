d1e88ad76146804d0563cbd150fee0c260f0a489
Pirapat Papat <pirapat-p@rmutp.ac.th>
2025-09-08 08:45:40 +0700

    server

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,5 @@
 import { serve } from "@hono/node-server";
-import app from "./index";
-
-serve(app,(info)=>{
-    console.log(`Sever is runnig on http://localhost:${info.port}`);
+import app from "./index.js"
+serve(app, (info) => {
+  console.log(`Server is running at http://localhost:${info.port}`);
 });
